bf0d938f732d002caf1507abfe88dafbda11f355
jessica cechin <jessica.cechin@gmail.com>
2019-07-17 17:30:27 -0300

    "Login"

---
+++ config/busca_cli_cpf.php
@@ -0,0 +1,22 @@
+<?php
+require 'conexao.php';
+$sql = "SELECT * FROM pessoa";
+
+$cpf=$_POST['q']; //recupera o parâmetro passado na visualização
+if ($cpf!='') {      
+    $sql.=" WHERE cpf = ('$cpf')";   
+
+}
+
+$result=mysqli_query($conexao,$sql);
+
+//echo mysqli_error($con);
+if (mysqli_affected_rows($conexao)>0) {
+    echo "1";
+} else {
+    echo "0";
+}
+
+mysqli_close($conexao);
+
+?>
--- index.php
+++ index.php
@@ -12,75 +12,25 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Login - Contratos</title>
 	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
-	<style>
-		html,
-		body {
-			height: 100%;
-		}
-
-		body {
-			display: -ms-flexbox;
-			display: flex;
-			-ms-flex-align: center;
-			align-items: center;
-			padding-top: 40px;
-			padding-bottom: 40px;
-			background-color: #f5f5f5;
-		}
-
-		.form-signin {
-			width: 100%;
-			max-width: 330px;
-			padding: 15px;
-			margin: auto;
-		}
-
-		.form-signin .checkbox {
-			font-weight: 400;
-		}
-
-		.form-signin .form-control {
-			position: relative;
-			box-sizing: border-box;
-			height: auto;
-			padding: 10px;
-			font-size: 16px;
-		}
-
-		.form-signin .form-control:focus {
-			z-index: 2;
-		}
-
-		.form-signin input[type="email"] {
-			margin-bottom: -1px;
-			border-bottom-right-radius: 0;
-			border-bottom-left-radius: 0;
-		}
-
-		.form-signin input[type="password"] {
-			margin-bottom: 10px;
-			border-top-left-radius: 0;
-			border-top-right-radius: 0;
-		}
-
-	</style>
+	<link rel="stylesheet" href="css/csslogin.css">
+	<link rel="icon" href="image/iconcotract.png">
 </head>
 
 <body class="text-center">
-	<form class="form-signin">
-		<h1 class="h3 mb-3 font-weight-normal">Faça login</h1>
-		<label for="usuario" class="sr-only">Endereço de email</label>
-		<input type="email" id="usuario" class="form-control" placeholder="Seu email" required autofocus>
-		<label for="inputPassword" class="sr-only">Senha</label>
-		<input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
-		<div class="checkbox mb-3">
-			<label>
-				<input type="checkbox" value="remember-me"> Lembrar de mim
-			</label>
-		</div>
-		<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-		<p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-	</form>
+	<div class="bloco sombra">
+		<form class="form-signin">
+			<img src="image/imagecontract.png" class="img-fluid" style="width:20%; height:10%;">
+			<h1 class="h3 mb-3 font-weight-normal">Faça login</h1>
+			<label for="usuario" class="sr-only">Endereço de email</label>
+			<input type="email" id="usuario" class="form-control" placeholder="Seu email" required autofocus>
+			<label for="inputPassword" class="sr-only">Senha</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+			<button class="btn btn-lg btn-secundary btn-block" type="button">Cadastre-se</button>
+		</form>
+
+	</div>
+
 	<script src="./node_modules/jquery/dist/jquery.slim.min.js"></script>
 	<script src="./node_modules/popper.js/dist/popper.min.js"></script>
 	<script src="./node_modules/bootstrap/dist/js/bootstrap.js"></script>
