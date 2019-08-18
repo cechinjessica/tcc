337225915feffe2593a86c022d6a226fb4247f38
jessica cechin <jessica.cechin@gmail.com>
2019-08-18 17:15:54 -0300

    contrato = vend

--- Contratos/config/busca_cli_contrato_comp.php
+++ Contratos/config/busca_cli_contrato_comp.php
@@ -1,18 +1,18 @@
-    <?php
-    require 'conexao.php';
-    $sql = "SELECT * FROM pessoa";
+<?php
+require 'conexao.php';
+$sql = "SELECT * FROM pessoa";
 
-    $nm=$_GET['q']; //recupera o parâmetro passado na visualização
+$nm=$_GET['q']; //recupera o parâmetro passado na visualização
+if ($nm!='') {
     if ($nm!='') {
-        if ($nm!='') {
-            $sql.=" WHERE UPPER(NOME) LIKE UPPER('%$nm%')";
-        }
+        $sql.=" WHERE UPPER(NOME) LIKE UPPER('%$nm%')";
     }
-    $sql.=" ORDER BY idpessoa";
+}
+$sql.=" ORDER BY idpessoa";
 
-    $result=mysqli_query($conexao,$sql);
-    if (mysqli_affected_rows($conexao)>0) {
-        echo "<div class='table-responsive'>
+$result=mysqli_query($conexao,$sql);
+if (mysqli_affected_rows($conexao)>0) {
+    echo "<div class='table-responsive'>
         <table class='table table-hover table-light table-sm' >
                  <thead>
                   <tr>
@@ -39,29 +39,29 @@
                     <th scope='col'>Operação</th>
                  <tr>
                 </thead>";
-        while ($row=mysqli_fetch_row($result))
-        {
+    while ($row=mysqli_fetch_row($result))
+    {
 
-            echo " <tbody class='table-striped'>";
-            echo " <tr>";
-            echo "<th scope='row' id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."') >".$row[0]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[1]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[2]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[3]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[4]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[5]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[6]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[7]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[9]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[8]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[10]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[11]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[12]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[19]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[13]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[14]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[15]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[16]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[17]."</td>";
-    echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[18]."</td>";
-    echo "<td> <a href=cadastros/vendedor.php?id=".$row[0]."&op=A".">Editar". "|" ."<a href=cadastros/vendedor.php?id=".$row[0]. "&op=D" . ">Apagar" . "</td>" ; echo " </tr>" ; } echo " </tbody>" ; echo "</table></div>" ; } mysqli_close($conexao); ?>
+        echo " <tbody class='table-striped'>";
+        echo " <tr>";
+        echo "<th scope='row' id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."') >".$row[0]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[1]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[2]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[3]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[4]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[5]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[6]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[7]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[9]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[8]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[10]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[11]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[12]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[19]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[13]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[14]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[15]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[16]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[17]."</td>";
+        echo "<td id=".$row[0]." onclick=getidcomp(".$row[0].",'".$row[7]."')>".$row[18]."</td>";
+        echo "<td> <a href=cadastros/vendedor.php?id=".$row[0]."&op=A".">Editar". "|" ."<a href=cadastros/vendedor.php?id=".$row[0]. "&op=D" . ">Apagar" . "</td>" ; echo " </tr>" ; } echo " </tbody>" ; echo "</table></div>" ; } mysqli_close($conexao); ?>
