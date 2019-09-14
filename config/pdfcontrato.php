fed39686b464713d9f59b586578a7b757ef0bb4d
jessica cechin <jessica.cechin@gmail.com>
2019-09-14 19:09:19 -0300

    dompdf

--- config/pdfcontrato.php
+++ config/pdfcontrato.php
@@ -1,69 +1,55 @@
 <?php
 
 //referenciar o DomPDF com namespace
-	use Dompdf\Dompdf;
+use Dompdf\Dompdf;
 
 require_once 'dompdf/autoload.inc.php';
 
-	include_once("conexao.php");
-	$html = "<div class='table-responsive'>
-	<table class='table table-sm table-bordered'>";
-	$html .= '<thead>';
-	$html .= '<tr>';
-	$html .= '<th >Nome Comprador</th>';
-	$html .= '<th >Nome Vendedor</th>';
-	$html .= '<th >Veículo</th>';
-	$html .= '<th >Placa</th>';
-	$html .= '<th >Valor</th>';
-	$html .= '<th >Quant. Parc.</th>';
-	$html .= '<th >Valor Parc.</th>';
-	$html .= '<th >Nome 1ª Testemunha </th>';
-	$html .= '<th >Nome 2ª Testemunha </th>';
-	$html .= '</tr>';
-	$html .= '</thead>';
-	$html .= '<tbody>';
+include_once("conexao.php");
+$html = "";
 
-	$sql = "SELECT p.nome as NomeComp, pp.nome as NomeVend, v.nome as NomeVei, v.placa, v.valor, c.numeroparcelas, c.valorparcela, c.nometestemunha1, c.nometestemunha2 FROM contrato c
+if (isset($_GET['id'])){
+	$id=$_GET['id'];
+	$html="<h1>Contrato ".$id."</h1></br>";
+}
+
+$sql = "SELECT p.* FROM contrato c
 	inner join pessoa p on c.Pessoa_IdComprador = p.idpessoa
 	inner join pessoa pp on c.Pessoa_IdVendedor = pp.idpessoa
-	inner join veiculo v on c.Veiculo_IdVeiculo = v.idveiculo";
-	$result = mysqli_query($conexao, $sql);
-	while($row = mysqli_fetch_array($result)){
-		$html .= '<tr><td>'.$row['NomeComp'] . "</td>";
-		$html .= '<td>'.$row['NomeVend'] . "</td>";
-		$html .= '<td>'.$row['NomeVei'] . "</td>";
-		$html .= '<td>'.$row[3] . "</td>";
-		$html .= '<td>'.$row[4] . "</td>";
-		$html .= '<td>'.$row[5] . "</td>";
-		$html .= '<td>'.$row[6] . "</td>";
-		$html .= '<td>'.$row[7] . "</td>";
-		$html .= '<td>'.$row[8] . "</td></tr>";
-	}
-
-	$html .= '</tbody>';
-	$html .= '</table></div>';
+	inner join veiculo v on c.Veiculo_IdVeiculo = v.idveiculo where c.idcontrato = $id";
+$result = mysqli_query($conexao, $sql);
+while($row = mysqli_fetch_array($result)){
+	$html .= $row[0];
+	$html .= $row[1];
+	$html .= $row[2];
+	$html .= $row[3] ;
+	$html .= $row[4] ;
+	$html .= $row[5] ;
+	$html .= $row[6] ;
+	$html .= $row[7] ;
+	$html .= $row[8];
+}
 
 
-	//Criando a Instancia
-	$dompdf = new DOMPDF();
-	// Carrega seu HTML
-	$dompdf->load_html('
-			<h1 style="text-align: center;">Celke - Relatório de Transações</h1>
-			'. $html .'
-		');
 
 
-$dompdf -> set_option ( ' defaultFont ' , 'sanserif' );
+//Criando a Instancia
+$dompdf = new DOMPDF();
+// Carrega seu HTML
+$dompdf->load_html( $html);
+
+
+$dompdf -> set_option ( 'defaultFont ' , 'sanserif' );
 $dompdf -> setPaper ( ' A4 ' , ' landscape ' );
 
-	//Renderizar o html
-	$dompdf->render();
+//Renderizar o html
+$dompdf->render();
 
-	//Exibibir a página
-	$dompdf->stream(
-		"relatorio_celke.pdf",
-		array(
-			"Attachment" => false //Para realizar o download somente alterar para true
-		)
-	);
+//Exibibir a página
+$dompdf->stream(
+	"contrato.pdf",
+	array(
+		"Attachment" => false //Para realizar o download somente alterar para true
+	)
+);
 ?>
